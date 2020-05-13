f6a720dfcff1a785160eba63e6bf40eb53c62756
John Blume <jlblumework@gmail.com>
2020-05-12 22:46:02 -0400

    Added connection stuff

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2')
 
-const defaultConfig = {
+let defaultConfig
+
+if (process.env.JAWSDB_URL){
+  defaultConfig = process.env.JAWSDB_URL
+}
+
+else {
+  defaultConfig = {
   host: 'localhost',
   user: 'root',
   password: 'HeatLight2Cage',
   database: 'burgers_db'
+  }
 }
 
 module.exports = mysql.createConnection(defaultConfig).promise()
